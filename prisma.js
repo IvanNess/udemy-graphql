d7ac135556fcaddade868482220b950ce0556550
IvanNess <chai@bk.ru>
2020-04-02 14:49:18 +0300

    Extract prisma secret to env var

--- prisma.js
+++ prisma.js
@@ -5,7 +5,7 @@ const {fragmentReplacements} = require('./resolvers')
 const prisma = new Prisma({
     typeDefs: 'generated/prisma.graphql',
     endpoint: process.env.PRISMA_ENDPOINT,
-    secret: 'thisismysupersecrettext',
+    secret: process.env.PRISMA_SECRET,
     fragmentReplacements
 
 })
